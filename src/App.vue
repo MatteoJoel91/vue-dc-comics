abec58f8be804de2f567bffcbb6ba0742b212cec
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-21 15:10:31 +0100

    push inizio

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MyHeader></MyHeader>
+    <MyMain></MyMain>
+    <MyFooter></MyFooter>
+    
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyMain from './components/MyMain.vue'
+import MyFooter from './components/MyFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyMain,
+    MyFooter
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+}
+</script>
+
+<style>
+
+</style>
